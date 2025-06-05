1704b82c16a2d57425f50df5b040842c83af1632
sirius <siriusblack7c@gmail.com>
2025-06-05 16:05:08 +0900

    complete -v3.1

--- frontend/src/components/StepCard.jsx
+++ frontend/src/components/StepCard.jsx
@@ -20,7 +20,7 @@ function StepCard({
                             {step.value && <div><strong>Value:</strong> {step.value}</div>}
                         </div>
                         {testing && currentStep === index && (
-                            <svg className="animate-spin h-5 w-5 !text-indigo-500 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                            <svg className="animate-spin h-5 w-5 !text-indigo-100 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                 <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                                 <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                             </svg>
@@ -77,7 +77,7 @@ function StepCard({
                 </div>
             </div>
             {result && (
-                <div className={`mt-2 text-sm ${result.status === 'success' ? 'text-green-600' : 'text-red-900'}`}>
+                <div className={`mt-2 text-sm ${result.status === 'success' ? 'text-gray-200 bg-green-500 w-[80px] text-center rounded-md' : 'text-gray-200 bg-red-500 w-[80px] text-center rounded-md'}`}>
                     {result.status === 'success' ? '✅ Success' : `❌ Failed`}
                     {result.evidence && (
                         <div className="mt-1">
